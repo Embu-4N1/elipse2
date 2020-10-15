184ea1bdcbd4871252cdd9acccbc25b438ecfa40
Leandro Chaves <leandro.rocha@akna.com>
2020-10-15 19:34:08 -0300

    First commit with quasar

---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'calc', component: () => import('pages/Calc.vue') },
+    ]
+  },
+
+  // {
+  //   path: '/calc',
+  //   component: () => import('pages/Calc'),
+  // },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
